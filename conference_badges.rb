2194d7af61e305af61510584cda014b3901fb459
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-26 19:00:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
-  names.collect do |name|
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
     badge_maker(name)
   end
 end
